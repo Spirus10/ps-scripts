f44596fda542918e9c6e5eb723fed1a8eb68df17
Spirus10 <bailey.evanoff@gmail.com>
2021-07-16 14:07:33 -0700

    typo

--- winUpdateFrozenFix.ps1
+++ winUpdateFrozenFix.ps1
@@ -1,1 +1,1 @@
-net stop wuauserv; net stop bits; net stop cryptsvc; ren  $env:SystemRoot\System32\Catroot2 Catroot2.old; ren %$env:SystemRoot\SoftwareDistribution SoftwareDistribution.old; net start wuauserv; net start bits; net start cryptsvc
+net stop wuauserv; net stop bits; net stop cryptsvc; ren  $env:SystemRoot\System32\Catroot2 Catroot2.old; ren $env:SystemRoot\SoftwareDistribution SoftwareDistribution.old; net start wuauserv; net start bits; net start cryptsvc
